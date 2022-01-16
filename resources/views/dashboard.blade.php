73c0ccbdb3d5a3b4b3ae7aae709ffa95a35492af
tananyapat <60129098+tananyapat@users.noreply.github.com>
2022-01-16 15:20:40 +0700

    scroll bar style

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -34,7 +34,36 @@
                 display:flex;
                 margin-left: 580px;
             }
+
+            ::-webkit-scrollbar {
+                width: 10px;
+ 
+            }
+
+            /* Track */
+            ::-webkit-scrollbar-track {
+                background: #f1f1f1; 
+                border-radius:10px;
+            }
+            
+                
+                /* Handle */
+            ::-webkit-scrollbar-thumb {
+                background: #888; 
+                border-radius:10px;
+
+            }
+            
+
+                /* Handle on hover */
+            ::-webkit-scrollbar-thumb:hover {
+                background: #555; 
+
+            }
+            
+
         </style>
+
     </head>
 
     
